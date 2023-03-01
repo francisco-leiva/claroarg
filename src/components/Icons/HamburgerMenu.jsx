b18c54a2fa5b01baf105530944c6020777f55001
francisco-leiva <leivafrancisco631@gmail.com>
2023-03-01 02:04:30 -0300

    Correcting svgs

--- src/components/Icons/HamburgerMenu.jsx
+++ src/components/Icons/HamburgerMenu.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 const HamburgerMenu = () => {
-  return(
+  return (
     <svg
-      fill="#fff"
-      ariaHidden="true"
-      className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv"
-      data-testid="MenuIcon"
-      viewBox="0 0 24 24"
-      style={{ width: 24, height: 24 }}
+      className='MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-vubbuv'
+      viewBox='0 0 24 24'
+      aria-hidden='true'
+      data-testid='MenuIcon'
+      style={{
+        fill: '#fff',
+        width: 24,
+        height: 24,
+      }}
     >
-      <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+      <path d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' />
     </svg>
-  )
-}
+  );
+};
 
 export default HamburgerMenu;
