e7ffab3915e3770acf5deb1545c4b153d62f7665
francisco-leiva <leivafrancisco631@gmail.com>
2023-05-11 00:27:00 -0300

    Fix image usage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import Carousel from 'react-bootstrap/Carousel';
-import {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
   collection,
   getDocs,
@@ -32,12 +32,16 @@ const Home = () => {
 
     // setting cellphones
     getDocs(cellphonesCollection).then((snapshot) => {
-      setCellphones(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})));
+      setCellphones(
+        snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
     });
 
     // setting accessories
     getDocs(accessoriesCollection).then((snapshot) => {
-      setAccessories(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})));
+      setAccessories(
+        snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      );
     });
   }, []);
 
@@ -45,25 +49,43 @@ const Home = () => {
     <>
       <Carousel>
         <Carousel.Item>
-          <img
-            className='d-block w-100 carouselItem__1'
-            src='https://images.ctfassets.net/weuwbjv1v9lc/5nOszX8ZwSjbHxtzq9naaY/031b4d21169713f03f5628b209fc5cdb/descuento-celulares-accesorios-45-tienda-claro-desktop.webp'
-            alt='Descuento 45% celulares y accesorios'
-          />
+          <picture>
+            <source
+              media='(max-width: 845px)'
+              srcSet='https://images.ctfassets.net/weuwbjv1v9lc/6Y8Gs9kv0STCRuqfb7eByB/751c0fc76c8337a93941f7142a5ed841/descuento-celulares-accesorios-40-tienda-claro-mobile.webp'
+            />
+            <img
+              className='d-block w-100'
+              src='https://images.ctfassets.net/weuwbjv1v9lc/5nOszX8ZwSjbHxtzq9naaY/031b4d21169713f03f5628b209fc5cdb/descuento-celulares-accesorios-45-tienda-claro-desktop.webp'
+              alt='Descuento 45% celulares y accesorios'
+            />
+          </picture>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className='d-block w-100 carouselItem__2'
-            src='https://images.ctfassets.net/weuwbjv1v9lc/4m0bFuLcCj0WGqbg55eHbU/8d4ed942a548a78725cfaf1c11d4e0af/moto-edge-30-ultra-neo-12-cuotas-tienda-claro-desktop.webp'
-            alt='12 cuotas tienda claro'
-          />
+          <picture>
+            <source
+              media='(max-width: 845px)'
+              srcSet='https://images.ctfassets.net/weuwbjv1v9lc/5bugJ9tmaTbtjwBCbJOYOB/226a485b8d78d742bcfb0be49d963e9f/moto-edge-30-ultra-neo-12-cuotas-tienda-claro-mobile.webp'
+            />
+            <img
+              className='d-block w-100'
+              src='https://images.ctfassets.net/weuwbjv1v9lc/4m0bFuLcCj0WGqbg55eHbU/8d4ed942a548a78725cfaf1c11d4e0af/moto-edge-30-ultra-neo-12-cuotas-tienda-claro-desktop.webp'
+              alt='12 cuotas tienda claro'
+            />
+          </picture>
         </Carousel.Item>
         <Carousel.Item>
-          <img
-            className='d-block w-100 carouselItem__3'
-            src='https://images.ctfassets.net/weuwbjv1v9lc/1bWgGnliGUsLuAPz4sj4IH/ea0ec02408d6e7fe7eeb0cbc29ef5b42/galaxy-s23-series-lanzamiento-tienda-claro-desktop.webp'
-            alt='12 cuotas galaxy watch y buds2 pro'
-          />
+          <picture>
+            <source
+              media='(max-width: 845px)'
+              srcSet='https://images.ctfassets.net/weuwbjv1v9lc/3UiWrzUkcMpUidY5zXgDF6/ead9af183225169d0c95d678c3abdfc4/galaxy-s23-series-lanzamiento-tienda-claro-mobile.webp'
+            />
+            <img
+              className='d-block w-100'
+              src='https://images.ctfassets.net/weuwbjv1v9lc/1bWgGnliGUsLuAPz4sj4IH/ea0ec02408d6e7fe7eeb0cbc29ef5b42/galaxy-s23-series-lanzamiento-tienda-claro-desktop.webp'
+              alt='12 cuotas galaxy watch y buds2 pro'
+            />
+          </picture>
         </Carousel.Item>
       </Carousel>
 
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,11 +4,17 @@ import ItemList from '../ItemList/ItemList';
 const ItemListContainer = ({ productsList, categories }) => {
   return (
     <>
-      <img
-        src='https://images.ctfassets.net/weuwbjv1v9lc/2K87mIHbP6vUHj1AzwTfAz/1bfcaf7a53b8c23118b09561d1f7b8d3/protege-tu-celu-tienda-claro-45-desktop.webp'
-        alt='Protege tu celular'
-        className='d-block w-100 principalImage'
-      />
+      <picture>
+        <source
+          media='(max-width: 845px)'
+          srcset='https://images.ctfassets.net/weuwbjv1v9lc/36cNY9jIIsAAswkD3BbLBG/ee43dd54588122037745533119cf5f29/protege-tu-celu-tienda-claro-mobile.webp'
+        />
+        <img
+          className='d-block w-100'
+          src='https://images.ctfassets.net/weuwbjv1v9lc/2K87mIHbP6vUHj1AzwTfAz/1bfcaf7a53b8c23118b09561d1f7b8d3/protege-tu-celu-tienda-claro-45-desktop.webp'
+          alt='Protege tu celular'
+        />
+      </picture>
 
       <div className='itemListContainer'>
         <div className='itemListContainer__categories'>
